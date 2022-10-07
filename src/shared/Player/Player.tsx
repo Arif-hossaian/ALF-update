438ed210939d58f34a75b582f072af15a70ee7e1
Arif-hossaian <arifhossain42136@gmail.com>
2022-10-07 21:42:47 +0600

    add sec musics but have storage prblm

--- src/shared/Player/Player.tsx
+++ src/shared/Player/Player.tsx
@@ -6,7 +6,7 @@ import Card from '../Card/Card';
 
 const Player = () => {
   return (
-    <div className="p-6 grid grid-cols-1 gap-10 md:grid-cols-3 lg:grid-cols-3 main-text mt-5">
+    <div className="p-2 grid grid-cols-1 gap-10 md:grid-cols-3 lg:grid-cols-3 main-text mt-5">
       {musicData?.map((data, id) => (
         <Card key={id} className="shadow-lg">
           <NcImage
@@ -14,11 +14,9 @@ const Player = () => {
             alt="Abdul_alim_Cover"
             className="object-cover object-center w-full transform transition-all hover:scale-105"
           />
-          <p className="m-4">
-            <span className="text-gray-500">Title:-</span>{' '}
-            <span className="en-font text-lg">{data.title}</span>
-          </p>
-          <ReactAudioPlayer src={data.music} controls />
+          <div className="mx-auto max-w-screen-sm mt-5">
+            <ReactAudioPlayer src={data.music} controls />
+          </div>
         </Card>
       ))}
     </div>
