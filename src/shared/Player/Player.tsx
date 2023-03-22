3d6a9789071c0c361df4afaad4fee7ca221b3189
Arif-hossaian <arifhossain42136@gmail.com>
2023-03-22 23:50:50 +0600

    fix multiple song play at a time issue

--- src/shared/Player/Player.tsx
+++ src/shared/Player/Player.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, createRef, useState } from 'react';
 import { musicData } from '../../constants/musicData';
 //import ReactAudioPlayer from 'react-audio-player';
 import AudioPlayer from 'react-h5-audio-player';
@@ -7,26 +7,38 @@ import NcImage from '../NcImage/NcImage';
 import Card from '../Card/Card';
 
 const Player: FC = () => {
+  const [currentSong, setCurrentSong] = useState<any>({});
+  const refs: any = {};
+
+  const handlePlay = (song: any) => {
+    //console.log(song);
+    if (currentSong.id && currentSong.id !== song.id) {
+      refs[currentSong.id].current.audio.current.pause();
+    }
+    setCurrentSong(song);
+  };
   return (
     <div className="p-2 grid grid-cols-1 gap-10 md:grid-cols-3 lg:grid-cols-3 main-text mt-5">
-      {musicData?.map((data, id) => (
-        <Card key={id} className="shadow-lg">
-          <NcImage
-            src="https://res.cloudinary.com/arifscloud/image/upload/v1657362446/alf-images/A-24_zegigt.jpg"
-            alt="Abdul_alim_Cover"
-            className="object-cover object-center w-full transform transition-all hover:scale-105"
-          />
-          <p className="m-3">{data.title}</p>
-          <div className="mx-auto max-w-screen-sm mt-5s">
-            {/* <ReactAudioPlayer src={data.music} controls /> */}
-            <AudioPlayer
-              src={data.music}
-              onPlay={(e) => console.log('onPlay')}
-              // other props here
+      {musicData?.map((data, id) => {
+        refs[data.id] = createRef<any>();
+        return (
+          <Card key={id} className="shadow-lg">
+            <NcImage
+              src="https://res.cloudinary.com/arifscloud/image/upload/v1657362446/alf-images/A-24_zegigt.jpg"
+              alt="Abdul_alim_Cover"
+              className="object-cover object-center w-full transform transition-all hover:scale-105"
             />
-          </div>
-        </Card>
-      ))}
+            <p className="m-3">{data.title}</p>
+            <div className="mx-auto max-w-screen-sm mt-5s">
+              <AudioPlayer
+                ref={refs[data.id]}
+                src={data.music}
+                onPlay={() => handlePlay(data)}
+              />
+            </div>
+          </Card>
+        );
+      })}
     </div>
   );
 };
